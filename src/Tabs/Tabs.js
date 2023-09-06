309d1e3b9fcbba261513297b473917e7383d562e
mrybaruk <mykola.rybaruk@pergamonmed.com>
2023-09-06 17:51:26 +0200

    tabs

--- src/Tabs/Tabs.js
+++ src/Tabs/Tabs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PatientList from '../PatientList/PatientList';
+import StudyList from '../StudyList/StudyList';
 import './Tabs.module.css';
 
 
@@ -7,6 +8,9 @@ import './Tabs.module.css';
 
 export default function Tabs(props) {
     return <div>
+        <button>Study Manager</button>
+        <button>Patients</button>
         <PatientList />
+        <StudyList />
     </div>
 }
